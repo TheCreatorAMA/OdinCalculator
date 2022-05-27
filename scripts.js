5961eea55c9979006e7ea302db2956866cbe8c24
TheCreatorAMA <alexanderakiu@gmail.com>
2022-05-27 07:20:03 -0700

    Add data-* attributes to buttons and add event listeners for operators/numbers

--- scripts.js
+++ scripts.js
@@ -1,9 +1,70 @@
-function add() {}
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+const displayEquation = document.querySelector('.display-equation');
+const displayResult = document.querySelector('.display-result');
 
-function subtract() {}
+displayResult.textContent = 'READY';
+displayEquation.textContent = '';
 
-function multiply() {}
+numberButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    if (displayResult.textContent == 'READY') displayResult.textContent = '';
+    displayResult.textContent += button.textContent;
+  });
+});
 
-function divide() {}
+operatorButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    switch (button.textContent) {
+      case 'A/C':
+        clearMemory();
+        break;
+      case '±':
+        console.log('±');
+        break;
+      case '%':
+        console.log('%');
+        break;
+      case '÷':
+        console.log('÷');
+        break;
+      case '×':
+        console.log('×');
+        break;
+      case '-':
+        console.log('-');
+        break;
+      case '+':
+        console.log('+');
+        break;
+      case '=':
+        console.log('=');
+        break;
+      default:
+        console.log('operator button pressed');
+    }
+  });
+});
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
 
 function operate() {}
+
+function clearMemory() {
+  displayEquation.textContent = '';
+  displayResult.textContent = '';
+}
